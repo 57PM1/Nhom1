4453e00aa569052770a8233303fc460dda543ef2
Nguyễn Văn Toàn <toanthree@gmail.com>
2016-06-14 20:12:58 +0700

    Fixed commit bug of Mr. Linh

--- QL_Thu_Vien/QL_Thu_Vien.Designer.cs
+++ QL_Thu_Vien/QL_Thu_Vien.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'F:\Project\QuanLyThuVien\QL_Thu_Vien\QL_Thu_Vien.edmx'. 
+﻿// T4 code generation is enabled for model 'J:\DA_CSDL\QuanLyThuVien\QL_Thu_Vien\QL_Thu_Vien.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
